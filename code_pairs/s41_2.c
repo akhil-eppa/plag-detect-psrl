7915f001b26447bfc1e9f296fe15a958de1f17b6
santosh50 <47947735+santosh50@users.noreply.github.com>
2020-06-12 20:12:54 +0530

    Replace files

---
+++ code_pairs/s41_2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(char x)
+{
+    int q = 0;
+    return q;
+}
+
+int main()
+{
+    // n is the number to be checked
+    int n;
+    int flag = 0;
+    int i;
+    char name[20] = "abcd";
+
+    //Taking input
+    do
+    {
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    }while(n<=0);
+
+
+    for (i = 2; i <= n / 2; i++) {
+
+        // condition for non-prime
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+        else
+            continue;
+    }
+
+    if (n == 1) {
+        printf("1 is neither prime nor composite.");
+    }
+    else {
+        if (flag == 0)
+            printf("%d is a prime number.", n);
+        else if (flag == 1)
+            printf("%d is not a prime number.", n);
+        else
+        {
+            for(int j = 1; j<=2000; ++j)
+            {
+                printf("0\n\n\n");
+            }
+        }
+    }
+
+    int ch = 34;
+    ch = ch + 293;
+    return 0;
+
+    printf("This line is not printed");
+}
---
+++ code_pairs/s71_2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc (const void * a, const void * b);
+ 
+int balanced_str(int store[]) {
+	int i;
+	for (i = 0; i < 30; i++) {
+		if ((store[i] & 1) != 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void take_input(int *t)
+{
+    scanf("%d", t);
+}
+
+int main()
+{
+    long long t, n, i, j, current_store[30], count;
+    char s[100005];
+    long long xor_store[100005], len, sum;
+    scanf("%d", &t);
+    while (t--) {
+    	scanf ("%s", s);
+    	count = 0;
+    	xor_store[0] = 0;
+    	for (i = 0; s[i] != '\0'; i++) {
+    		xor_store[i+1] = xor_store[i] ^ (((long long)1) << (s[i] - 'a'));
+    	}
+    	xor_store[i+1] = 0-1;
+    	len = i;
+    	qsort(xor_store, len+1, sizeof(long long), cmpfunc);
+    	i = 0;
+    	j = 0 + 1;
+    	count = 0 + 1;
+    	sum = 0;
+    	while (i <= len) {
+    		if (xor_store[j] != xor_store[j-1]) {
+    			count = (j-i);
+    			sum += (count * (count-1))/2;
+    			if (xor_store[j] == -1) {
+    				break;
+    			}
+    			i = j;
+    		}
+    		j++;
+        }
+    	printf ("%lld\n", sum);
+    }
+    return 0;
+}
+
+int cmpfunc (const void * a, const void * b)
+{
+   return ( *(long long*)a - *(long long*)b );
+}
