86d75b6d140d01bc18d536053c3d72d163cf9fa4
Akhil Eppa <akhil.e2k@gmail.com>
2020-06-13 15:57:53 +0530

    Added PCA models

--- ML_models/SVM_pca.py
+++ ML_models/SVM_pca.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 12 11:31:22 2020
+Created on Sat Jun 13 15:30:40 2020
 
 @author: Akhil
 """
+
 
 from sklearn.metrics import confusion_matrix,classification_report
 from sklearn import preprocessing
@@ -12,11 +13,12 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
 '''
 LabelEncoder is used to label as 0 or 1 or 2 based on category
 '''
 labels=preprocessing.LabelEncoder()
-dataset_name=".csv"
+dataset_name="iris.csv"
 x=pd.read_csv(dataset_name)
 '''
 Shuffling the order of rows in file
@@ -28,17 +30,29 @@ Labels are the last column
 Features are all the remaining columns
 '''
 X=x.iloc[:,:-1]
+X=preprocessing.scale(X)
 mm_scaler = preprocessing.MinMaxScaler()
 X=mm_scaler.fit_transform(X)
 Y=x.iloc[:,-1:]
 #print(X)
 #print(Y)
 Y=labels.fit_transform(Y)
+'''
+Getting the principal components
+'''
+comp=2
+pca=PCA(n_components=comp)
+principalComponent=pca.fit_transform(X)
+cols=list()
+for i in range(comp):
+    cols.append('principal_comp_'+str(i+1))
+#Y=pd.DataFrame(Y)
+principalDf=pd.DataFrame(data=principalComponent,columns=cols)
 #print(Y)
 '''
 20% data is test data
 '''
-X_train, X_test, y_train, y_test = train_test_split(X, np.ravel(Y), test_size=0.2,random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(principalDf, np.ravel(Y), test_size=0.2,random_state=101)
 print(X_train.shape)
 print(y_train.shape)
 print(X_test.shape)
--- ML_models/knn_pca.py
+++ ML_models/knn_pca.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat Jun 13 14:22:21 2020
+
+@author: Akhil
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Jun  9 11:35:39 2020
 
 @author: Akhil
@@ -11,12 +18,14 @@ from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
 
 '''
 LabelEncoder is used to label as 0 or 1 or 2 based on category
 '''
 labels=preprocessing.LabelEncoder()
-dataset_name=".csv"
+dataset_name="iris.csv"
 x=pd.read_csv(dataset_name)
 '''
 Shuffling the order of rows in file
@@ -34,11 +43,24 @@ Y=x.iloc[:,-1:]
 #print(X)
 #print(Y)
 Y=labels.fit_transform(Y)
+'''
+Getting the principal components
+'''
+comp=2
+pca=PCA(n_components=comp)
+principalComponent=pca.fit_transform(X)
+cols=list()
+for i in range(comp):
+    cols.append('principal_comp_'+str(i+1))
+#Y=pd.DataFrame(Y)
+principalDf=pd.DataFrame(data=principalComponent,columns=cols)
+#finalDf=pd.concat([principalDf,Y],axis=1)
+#print(cols)
 #print(Y)
 '''
 20% data is test data
 '''
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(principalDf, Y.ravel(), test_size=0.2)
 print(X_train.shape)
 print(y_train.shape)
 print(X_test.shape)
@@ -52,10 +74,18 @@ like cosine similarity
 algorithm is set as suto
 '''
 model=KNeighborsClassifier(n_neighbors=7, weights='distance', algorithm='auto')
-model.fit(X_train,y_train)
+model.fit(X_train,y_train.ravel())
 y_pred=model.predict(X_test)
-print(y_test)
-print(y_pred)
+''' 
+importance = model.
+for i,v in enumerate(importance):
+	print('Feature: %0d, Score: %.5f' % (i,v))
+# plot feature importance
+pyplot.bar([x for x in range(len(importance))], importance)
+pyplot.show()
+'''
+#print(y_test)
+#print(y_pred)
 '''
 Print accuracy and the confusion matrix
 '''
