ae5cd4e96343415b9d36acbb868cfa038ff2ff46
Anirudh H M <anihm136@gmail.com>
2020-06-14 17:17:31 +0530

    Export to csv

--- extraction/extract_features_pipeline_train.py
+++ extraction/extract_features_pipeline_train.py
@@ -1,5 +1,8 @@
-import subprocess
+import csv
 import os
+import pickle
+import subprocess
+
 from merge_milepost_features import merge_features
 from text_features import generate_text_features
 
@@ -17,3 +20,59 @@ subprocess.run(
 merge_features(os.path.join("result_train", "merged_milepost_features_diff.pkl"), "train")
 generate_text_features()
 
+with open(os.path.join("result_train", "feat_v1.csv"), "w") as writefile:
+    mp_feat = pickle.load(
+        open(os.path.join("result_train", "merged_milepost_features_diff.pkl"), "rb")
+    )
+    txt_feat_1 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ad.pkl"), "rb")
+    )
+    txt_feat_2 = pickle.load(
+        open(os.path.join("result_train", "text_features", "cc_rat.pkl"), "rb")
+    )
+    txt_feat_3 = pickle.load(
+        open(os.path.join("result_train", "text_features", "cl_rat.pkl"), "rb")
+    )
+    txt_feat_4 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ed.pkl"), "rb")
+    )
+    txt_feat_5 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ld_rat.pkl"), "rb")
+    )
+    writer = csv.writer(writefile)
+    for features in zip(
+        mp_feat, txt_feat_1, txt_feat_2, txt_feat_3, txt_feat_4, txt_feat_5
+    ):
+        fl = []
+        fl.extend(features[0])
+        fl.extend(features[1:])
+        writer.writerow(fl)
+
+
+with open(os.path.join("result_train", "feat_v2.csv"), "w") as writefile:
+    mp_feat = pickle.load(
+        open(os.path.join("result_train", "merged_milepost_features_diff.pkl"), "rb")
+    )
+    txt_feat_1 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ad.pkl"), "rb")
+    )
+    txt_feat_2 = pickle.load(
+        open(os.path.join("result_train", "text_features", "cc.pkl"), "rb")
+    )
+    txt_feat_3 = pickle.load(
+        open(os.path.join("result_train", "text_features", "cl.pkl"), "rb")
+    )
+    txt_feat_4 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ed.pkl"), "rb")
+    )
+    txt_feat_5 = pickle.load(
+        open(os.path.join("result_train", "text_features", "ld.pkl"), "rb")
+    )
+    writer = csv.writer(writefile)
+    for features in zip(
+        mp_feat, txt_feat_1, txt_feat_2, txt_feat_3, txt_feat_4, txt_feat_5
+    ):
+        fl = []
+        fl.extend(features[0])
+        fl.extend(features[1:])
+        writer.writerow(fl)
