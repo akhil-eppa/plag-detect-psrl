f60178716e70fc5e59aa07c98b46496368135f71
Anirudh H M <anihm136@gmail.com>
2020-08-07 11:36:46 +0530

    Updated output format

--- src/main_alt.py
+++ src/main_alt.py
@@ -55,6 +55,7 @@ def main(args):
         raise ValueError("OUTPUT_FILE must be a csv file")
     if args.pairs_file:
         pairs = pickle.load(open(args.pairs_file, "rb"))
+        files = list(set(pair[0] for pair in pairs) + set(pair[1] for pair in pairs))
     else:
         files = listUtils.getSubmissions(root_dir)
         pairs = listUtils.getCombinations(files)
@@ -71,7 +72,6 @@ def main(args):
         )
     elif args.method == "codebert":
         y_pred = cbEval.evaluate(pairs, root_dir)
-    l = []
     if not args.train:
         with open(res_file, "w") as wr:
             writer = csv.writer(wr)
@@ -83,27 +83,30 @@ def main(args):
                     if pred:
                         print(f"{f1}  --->  {f2}")
             else:
-                writer.writerow(["File 1", "File 2", "Plagiarised", "Probability"])
-                for (f1, f2), prob, pred in zip(pairs, y_prob[:, 1], y_pred):
-                    writer.writerow([f1, f2, pred, prob])
+                l = []
+                writer.writerow(
+                    [
+                        "File",
+                        "Match 1",
+                        "Probability",
+                        "Match 2",
+                        "Probability",
+                        "Match 3",
+                        "Probability",
+                    ]
+                )
+                for (f1, f2), prob, _ in zip(pairs, y_prob[:, 1], y_pred):
                     l.append([f1, f2, prob])
-                    if pred:
-                        print(f"{f1}  --->  {f2}")
                 for i in files:
                     z = [x for x in l if x[0] == i or x[1] == i]
-                    z.sort(key=take_prob)
-                    if len(z) < 3:
-                        print("top matches of", i)
-                        for i in z:
-                            print(
-                                i[0], "and", i[1], "have", i[2] * 100, "% similarity."
-                            )
-                    else:
-                        print("top 3 matches of", i)
-                        for i in z:
-                            print(
-                                i[0], "and", i[1], "have", i[2] * 100, "% similarity."
-                            )
+                    z.sort(key=take_prob, reverse=True)
+                    temp = []
+                    temp.append(i)
+                    print("Top matches of", i)
+                    for j in z[0:3]:
+                        print(j[0], "and", j[1], "have", j[2] * 100, "% similarity.")
+                        temp.extend([j[1], j[2]])
+                    writer.writerow(temp)
                     print()
 
 
